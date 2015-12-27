4fbd744f72db799153a8749ebcf31afc02bed519
Vladimir Zahradnik <vladimir.zahradnik@gmail.com>
2015-12-27 20:37:12 +0100

    Implemented traversal through episodes.

--- default.py
+++ default.py
@@ -26,19 +26,20 @@ import xbmc, xbmcplugin, xbmcgui, xbmcaddon
 import urllib, urllib2, urlparse
 
 web_base_url = 'http://www.nebickovpapulke.sk'
-user_agent = 'Mozilla/5.0 (Windows NT 6.1; WOW64; rv:33.0) Gecko/20120101 Firefox/33.0'
-
 addon = xbmcaddon.Addon()
 addon_handle = int(sys.argv[1])
 args = urlparse.parse_qs(sys.argv[2][1:])
 
 xbmcplugin.setContent(addon_handle, 'movies')
 
+MODE_PLAY_EPISODE = 'play_episode'
+
 def build_url(query):
     addon_base_url = sys.argv[0]
     return addon_base_url + '?' + urllib.urlencode(query)
 
 def get_http_data_from_url(url):
+    user_agent = 'Mozilla/5.0 (Windows NT 6.1; WOW64; rv:33.0) Gecko/20120101 Firefox/33.0'
     request = urllib2.Request(url)
     request.add_header('User-Agent', user_agent)
     response = urllib2.urlopen(request)
@@ -59,7 +60,7 @@ def add_item(name, url, mode, isFolder=False):
     return result
 
 
-def add_directory(name, url, mode):
+def add_directory(name, url, mode=None):
     return add_item(name, url, mode, isFolder=True)
 
 
@@ -75,11 +76,28 @@ def list_archived_shows():
         url = url[0]
 
     content = get_http_data_from_url(url)
-    notify(url)
+
+    # Limit result to only Epizode Archive part
+    content = content[content.find('Archív Epizód') : content.find('<aside class="second">')]
+
+    # Regexp to match aired date and epizode name
+    pattern = 'class="date-display-single">(\d{2}.\d{2}.\d{4})<\/span>.*?<a href="(.*?)">Epizóda (\d{2}\/\d{4})\s*:*\s*(.*?)<\/a>'
+    match = re.compile(pattern, re.DOTALL).findall(content)
+
+    for aired, url, episode, name in match:
+        add_directory(translation(30000) + " " + episode + ": " + name, web_base_url + url, MODE_PLAY_EPISODE)
+
+    # Check if there is more episodes
+    pattern = 'class="pager-next.*?<a href="(.*?)"'
+    match = re.compile(pattern, re.DOTALL).findall(content)
+
+    if match:
+        url = match[0]
+        add_directory("[B]" + translation(30001) +"[/B]", web_base_url + url)
 
     xbmcplugin.endOfDirectory(addon_handle)
 
 mode = args.get('mode', None)
 
-if mode is None:
-    list_archived_shows()
+#if mode is None:
+list_archived_shows()
