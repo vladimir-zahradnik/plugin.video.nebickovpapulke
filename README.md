4b1c8570ea767875991b000a8d137a23f3248bc5
Vladimir Zahradnik <vladimir.zahradnik@gmail.com>
2016-03-26 13:55:48 +0100

    Created skeleton for ContentProvider

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
-# plugin.video.nebickovpapulke
+# plugin.video.nebickovpapulke.sk
 Kodi addon to play archived TV shows from http://www.nebickovpapulke.sk.
--- default.py
+++ default.py
@@ -1,6 +1,6 @@
 # -*- coding: UTF-8 -*-
 # *
-# *      Copyright (C) 2015 Vladimir Zahradnik
+# *      Copyright (C) 2016 Vladimir Zahradnik
 # *
 # *
 # *  This Program is free software; you can redistribute it and/or modify
@@ -19,3 +19,27 @@
 # *  http://www.gnu.org/copyleft/gpl.html
 # *
 # */
+import os
+sys.path.append( os.path.join ( os.path.dirname(__file__),'resources','lib') )
+import nebicko
+import xbmcprovider,xbmcaddon,xbmcutil,xbmc
+import util
+import traceback,urllib2
+
+__scriptid__   = 'plugin.video.nebickovpapulke.sk'
+__scriptname__ = 'nebickovpapulke.sk'
+__addon__      = xbmcaddon.Addon(id=__scriptid__)
+__language__   = __addon__.getLocalizedString
+
+settings = {'quality':__addon__.getSetting('quality')}
+params = util.params()
+if params=={}:
+    xbmcutil.init_usage_reporting(__scriptid__)
+provider = nebicko.NebickoContentProvider()
+
+class XBMCNebickoContentProvider(xbmcprovider.XBMCMultiResolverContentProvider):
+    #TODO: Implement listing
+    pass
+
+
+XBMCNebickoContentProvider(provider,settings,__addon__).run(params)
