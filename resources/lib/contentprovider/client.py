18dc68be5b4a3226a3078fae9521e4b5114ea0ed
Vladimir Zahradnik <vladimir.zahradnik@gmail.com>
2016-07-27 22:49:57 +0200

    Partial implementation of episode listing.

--- resources/lib/contentprovider/client.py
+++ resources/lib/contentprovider/client.py
@@ -19,10 +19,10 @@
 # *  http://www.gnu.org/copyleft/gpl.html
 # *
 # */
+from resources.lib.exceptions.ProviderException import ProviderException
+from resources.lib.http import HttpClient
 
 __author__ = 'vladimir.zahradnik'
-
-from resources.lib.http import HttpClient
 
 
 class Client(HttpClient):
@@ -37,4 +37,45 @@ class Client(HttpClient):
         self._items_per_page = items_per_page
 
     def _create_url(self, path):
-        return 'http://{0}/{1}'.format(unicode(self._host, 'utf-8'), unicode(path, 'utf-8'))
+        return 'http://{0}/{1}'.format(unicode(self._host, 'utf-8'), unicode(path.strip('/'), 'utf-8'))
+
+    @staticmethod
+    def _create_params(page=1):
+        params = {}
+        if page > 1:
+            params['page'] = str(page - 1)
+
+        return params
+
+    @staticmethod
+    def _handle_error(response):
+        status_class = response.status_code / 100
+        # accept 2XX and 3XX
+        if status_class != 2 and status_class != 3 and status_class != 5:
+            json_data = response.json()
+            error_message = 'HTTP error %d' % response.status_code
+            if 'error' in json_data:
+                error_message = json_data['error']
+                pass
+
+            if 'errors' in json_data:
+                errors = json_data['errors']
+                if len(errors) > 0:
+                    error_message = errors[0].get('error_message', '')
+                pass
+
+            # last fallback
+            if not error_message:
+                error_message = response.headers.get('status', error_message)
+                pass
+            raise ProviderException(error_message)
+        pass
+
+    def get_episodes(self, page=1):
+        params = self._create_params(page)
+        response = self._request(self._create_url('epizody'),
+                                 headers={'Accept': 'text/html'},
+                                 params=params)
+        self._handle_error(response)
+        # return items.convert_to_items(response.json())
+        return response
